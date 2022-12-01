7cd687c46e50e0761453c60adc769b0a77c8da85
Je-Carlos <cpjeanberg@gmail.com>
2022-12-01 17:39:52 -0300

    adicionando animação ao scroll da página

--- script.js
+++ script.js
@@ -63,3 +63,22 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+// Scroll Animation
+function initScrollAnimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowLoad = window.innerHeight * 0.5; // Calculo para pegar 50% da tela
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top; // Verificar aonde está as sections em relação a tela
+        isSectionVisible = sectionTop - windowLoad < 0;
+        // Quando o sectionTop for menor que 0
+        if (isSectionVisible) section.classList.add("active");
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initScrollAnimation();
