4dd393ac40e9ad38a287a3cc795b598d09bff922
Je-Carlos <cpjeanberg@gmail.com>
2022-12-01 11:35:06 -0300

    accordion list no FAQ e criando const active

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-const tabMenu = document.querySelectorAll(".js-tabMenu li");
-const tabContent = document.querySelectorAll(".js-tabContent section");
-tabContent[0].classList.add("active"); // Primeiro item sempre ativo
+// Navegação por tabs
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabMenu li");
+  const tabContent = document.querySelectorAll(".js-tabContent section");
+  const activeClass = "active";
+  tabContent[0].classList.add(activeClass); // Primeiro item sempre ativo
 
-// verificar se existe a tabMenu e tabContent, para não dar erro de carregamento.
-if (tabMenu.length && tabContent.length) {
-  // Função que adiciona a classe "active" ao elemento clicado e remove daqueles que não foram clicados.
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("active");
+  // verificar se existe a tabMenu e tabContent, para não dar erro de carregamento.
+  if (tabMenu.length && tabContent.length) {
+    // Função que adiciona a classe "active" ao elemento clicado e remove daqueles que não foram clicados.
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(activeClass);
+      });
+      tabContent[index].classList.add(activeClass);
+    }
+
+    // Função que adiciona o evento de click em cada imagem, quando clicada,
+    // chama a função "activeTab" passando o index da imagem clicada.
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("active");
   }
-
-  // Função que adiciona o evento de click em cada imagem, quando clicada,
-  // chama a função "activeTab" passando o index da imagem clicada.
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
+
+// Accordion List
+function initAccordionList() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "active";
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordionList();
