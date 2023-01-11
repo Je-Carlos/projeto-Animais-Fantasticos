4ae1c005a284d62c8afe7c484d45d5cc33139641
Je-Carlos <cpjeanberg@gmail.com>
2023-01-11 18:51:47 -0300

    update

--- src/js/modules/dropdown-menu.js
+++ src/js/modules/dropdown-menu.js
@@ -4,14 +4,14 @@ export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll("[data-dropdown]");
   dropdownMenus.forEach((menu) => {
     // Array contendo duas actions para usuario mobile e usuario pc
-    ["touchstart", "click"].forEach((userEvent) => {
+    ["click"].forEach((userEvent) => {
       menu.addEventListener(userEvent, handleClick);
     });
   });
   function handleClick(e) {
     e.preventDefault();
     this.classList.add("active");
-    outsideClick(this, ["touchstart", "click"], () => {
+    outsideClick(this, ["click"], () => {
       this.classList.remove("active");
     });
   }
